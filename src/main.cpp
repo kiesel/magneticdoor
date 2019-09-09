5447611bc3d027f82e519291f01b8845c986ac50
Alex D'Andrea <alex@dandrea.io>
2019-09-09 22:41:13 +0200

    Setup wifi using WifiManager

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <MQTT.h>
-#include <sensor.h>
+#include <WiFiManager.h>
+#include "sensor.h"
 
-const char SSID[] = "SSID";
-const char SSPW[] = "SSPW";
+
+const char* MQTT_HOST = "192.168.0.129";
 const String DEPLOYMENT_NAME = "prototype";
 
 WiFiClient net;
 MQTTClient mqtt;
+WiFiManager wifiManager;
 
 Sensor sensor = Sensor("door1", D1, D2);
 
@@ -19,7 +21,8 @@ void messageReceived(String &topic, String &payload) {
 }
 
 void mqttConnect() {
-  mqtt.begin("192.168.0.129", net);
+  Serial.printf("Attempting to connect to %s\n", MQTT_HOST);
+  mqtt.begin(MQTT_HOST, net);
   mqtt.onMessage(messageReceived);
   while (!mqtt.connect("esp8266", "try", "try")) {
     Serial.print(".");
@@ -30,24 +33,17 @@ void mqttConnect() {
 
 void notifyMqtt(String sensorName, int lastValue, int newValue, int count) {
     char payload[100];
-    sprintf(payload, "{ \"status\": %d, \"oldStatus\": %d, \"changes\": %d, millis: %d }", newValue, lastValue, count, millis());
+    sprintf(payload, "{ \"status\": %d, \"oldStatus\": %d, \"changes\": %d, millis: %d }", newValue, lastValue, count, (int)millis());
 
     mqtt.publish("sensor/" + DEPLOYMENT_NAME + "/" + sensorName, payload);
 }
 
 void setup() {
   Serial.begin(9600);
+  while (!Serial) {}
   Serial.println("Booting device...");
 
-  // Connect to network
-  if (WiFi.SSID() != SSID) {
-    WiFi.begin(SSID, SSPW);
-    while (WiFi.status() != WL_CONNECTED) {
-      Serial.print(".");
-      delay(1000);
-    }
-    Serial.println();
-  }
+  wifiManager.autoConnect();
 
   // Connect to mqtt
   mqttConnect();
